cbf2b66879afd24851b36f586a2139478b675e6b
Bastian Maerkisch <bmaerkisch@web.de>
2020-06-27 16:38:52 +0200

    Update to new library name

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -12,14 +12,13 @@ When a button is pressed, the backlight changes color.
 
 // include the library code:
 #include <Wire.h>
-#include <Adafruit_RGBLCDShield_Fast.h>
-#include <utility/Adafruit_MCP23017.h>
+#include <RGBLCDShield_Fast.h>
 
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
 // However, you can connect other I2C sensors to the I2C bus and share
 // the I2C bus.
-Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+RGBLCDShield_Fast lcd = RGBLCDShield_Fast();
 
 // These #defines make it easy to set the backlight color
 #define RED 0x1
